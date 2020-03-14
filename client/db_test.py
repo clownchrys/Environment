c098a1ed6e295bf3adfaa9adb883ced620040d79
clownchrys <clownchrys@gmail.com>
2020-03-15 02:35:44 +0900

    최초 작성: client, mysql, mongo 컨테이너 생성 및 인티그레이션

---
+++ client/db_test.py
@@ -0,0 +1,22 @@
+import os
+from pymongo import MongoClient
+from pymysql import connect
+
+# Mongo
+client = MongoClient(os.getenv("MONGO_URI"))
+client.database_names()
+
+# Mysql
+host, port = os.getenv("MYSQL_URI").split(":")
+conn = connect(
+    host=host,
+    port=port,
+    user="root",
+    password="",
+    db="mysql",
+    charset="utf8mb4",
+)
+
+cursor = conn.cursor()
+cursor.execute("show tables")
+cursor.fetchall()
