99a04b9755f18b9c27ca118c00474c195d16c561
clownchrys <clownchrys@gmail.com>
2020-03-20 10:08:42 +0900

    workspace_init.sh: client의 Workspace를 /Users/chan에 symlink하는 스크립트 추가

--- client/jupyter/jupyter_notebook_config.py
+++ client/jupyter/jupyter_notebook_config.py
@@ -8,7 +8,6 @@ c.NotebookApp.allow_remote_access = True
 c.NotebookApp.token = ""
 
 c.NotebookApp.open_browser = False
-c.NotebookApp.notebook_dir = "/root/workspace"
 
 # Configuration file for jupyter-notebook.
 
---
+++ workspace_init.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+SRC_DIR=$(pwd)/client/Workspace
+DEST_DIR=$HOME/Workspace
+ln -fhsv $SRC_DIR $DEST_DIR
